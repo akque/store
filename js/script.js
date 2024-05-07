81d5a540165b2d2515c1e87e160b02674174b490
akque <119449560+akque@users.noreply.github.com>
2024-05-07 18:00:29 +0300

    asd

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ import { getProducts } from "./api.js";
 const cont = document.querySelector(".container")
 const cartBtn = document.querySelector(".cartBtn")
 const cart = document.querySelector(".cart")
+const cartProductsArr = []
 
 cartBtn.addEventListener("click", openCart)
 
@@ -22,8 +23,42 @@ function renderProduct (obj) {
         <btn class="buy_btn">Buy</btn>
     `
     cont.append(card) // append - добавляет элемент в конец контейнера
+    const buyBtn = card.querySelector(".buy_btn")
+    buyBtn.addEventListener("click", () => {
+        const element = cartProductsArr.find((item) => item.id === obj.id)
+        if (element) {
+            element.count += 1
+            console.log(element.count)
+            refrestCartProduct(element)
+        }
+        else {
+            const newOvj = {...obj, count: 1}
+            cartProductsArr.push(newOvj)
+            createCartProduct(newOvj)
+        }
+    })
+}
+
+function createCartProduct (obj) {
+    const Product = document.createElement("div")
+    Product.classList.add("added_product")
+    Product.innerHTML = `
+    <h3>${obj.title}</h3>
+    <p>${obj.price}</p>
+    <div>
+        <button><-</button>
+        <span>${obj.count}</span>
+        <button>-></button>
+    </div>
+    `
+    obj.elemCount = Product.querySelector("span")
+    cart.append(Product)
+}
+
+function refrestCartProduct (element) {
+    element.elemCount.textContent = element.count
 }
 
 function openCart () {
-    cart.classList.add("cartOpened")
+    cart.classList.toggle("cartOpened")
 }
